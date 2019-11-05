a9450db87dffa19cd6694e0b5f879d1661896152
IvanMeng <37363504+IAmWhoAmI007@users.noreply.github.com>
2019-11-05 14:15:28 +0800

    (3)领域层定义仓储并实现

---
+++ aspnet-core/src/LearningAbp.EntityFrameworkCore/EntityFrameworkCore/Repositories/BackendTaskRepository.cs
@@ -0,0 +1,26 @@
+﻿using Abp.EntityFrameworkCore;
+using LearningAbp.IRepositories;
+using LearningAbp.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LearningAbp.EntityFrameworkCore.Repositories
+{
+    public class BackendTaskRepository : LearningAbpRepositoryBase<Task>, IBackendTaskRepository
+    {
+        public BackendTaskRepository(IDbContextProvider<LearningAbpDbContext> dbContextProvider) : base(dbContextProvider)
+        { }
+
+        public List<Task> GetTaskByAssignedPersonId(long personId)
+        {
+            var query = GetAll();
+
+            if(personId > 0)
+            {
+                query = query.Where(t => t.AssignedPersonId == personId);
+            }
+
+            return query.ToList();
+        }
+    }
+}
